6da13c9b7c4c6ddfcb954fe5f40b0d9aa7787012
a1xg <smirnov.oleksandr.dev@gmail.com>
2022-01-20 14:20:39 +0200

    Partially implemented logic for processing data from a file

--- meter/app_meter/forms.py
+++ meter/app_meter/forms.py
@@ -1,5 +1,6 @@
 import io
-import csv
+#import csv
+import pandas as pd
 from django.forms import ModelForm, Form, FileField, ValidationError
 from . import models
 
@@ -32,6 +33,7 @@ class ReadingsFileForm(Form):
     def clean_data_file(self):
         """Checking the file type"""
         file = self.cleaned_data['csv_file']
+        print('CLEAN DATA FILE')
         if file:
             ext = file.name.split('.')[-1]
             if ext != 'csv':
@@ -39,12 +41,19 @@ class ReadingsFileForm(Form):
             return file
 
     def process_data(self):
-        # TODO написать валидацию и бизнес логику
-        print(f'FORM{self.cleaned_data}')
         file = io.TextIOWrapper(self.cleaned_data['csv_file'].file)
-        print(f'file {file}')
-        data = csv.DictReader(file)
-        print(f'mobile {data}')
-        for i in data:
-            print(i)
+        date_colnames = ['DATE']
+        df = pd.read_csv(file, sep=',', parse_dates=date_colnames)
+        new_df = pd.DataFrame()
+        new_df['absolute_value'] = df['VALUE'].astype(int)
+        new_df['date'] = df['DATE']
+        new_df = new_df.sort_values(by='date')
+        new_df['relative_value'] = new_df['absolute_value'].diff().fillna(0)
+        new_df['relative_value'] = new_df['relative_value'].astype(int)
+        print(f'new df {new_df}')
+
+        # TODO написать сортировку, проверку формата записи, проверку наличия ошибок записи
+        #for index, item in enumerate(df):
+            #relative_val = int(df[index]['VALUE']) - int(df[index+1]['VALUE'])
+        #    print(f'item: {item}\nindex: {index}')
 
